a93820469c09e3b0cbb196e169ba549065ed5100
Le Pham Hoang Long <lphl.190520@gmail.com>
2024-12-07 08:32:46 +0700

    [Add] Add Basket service and Init Discount grpc service

---
+++ src/Services/Basket/Basket.API/Basket/StroreBasket/StroreBasketEndpoint.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Basket.API.Basket.StroreBasket
+{
+    public record StoreBasketRequest(ShoppingCart Cart);
+    public record StoreBasketResponse(String UserName);
+    public class StroreBasketEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/basket", async(StoreBasketRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<StroreBasketCommand>();
+
+                var result = await sender.Send(command);
+
+                var response = result.Adapt<StoreBasketResponse>();
+
+                return Results.Created($"/basket/{response.UserName}", response);
+            })
+            .WithName("StoreBasket")
+            .Produces<StoreBasketResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Store Basket")
+            .WithDescription("Store Basket");
+        }
+    }
+}
---
+++ src/Services/Basket/Basket.API/Basket/StroreBasket/StroreBasketHandler.cs
@@ -0,0 +1,27 @@
+﻿namespace Basket.API.Basket.StroreBasket
+{
+    public record StroreBasketCommand(ShoppingCart Cart) 
+        : ICommand<StoreBasketResult>;
+    public record StoreBasketResult(string UserName);
+    public class StroreBasketCommandValidator : AbstractValidator<StroreBasketCommand>
+    {
+        public StroreBasketCommandValidator()
+        {
+            RuleFor(x=>x.Cart).NotNull().WithMessage("Cart cannot be null!");
+            RuleFor(x=>x.Cart.UserName).NotEmpty().WithMessage("UserName is required!");
+        }
+    }
+    public class StroreBasketCommandHandler
+        (IBasketRepository repository)
+        : ICommandHandler<StroreBasketCommand, StoreBasketResult>
+    {
+        public async Task<StoreBasketResult> Handle(StroreBasketCommand command, CancellationToken cancellationToken)
+        {
+            await repository.StoreBasket(command.Cart, cancellationToken);
+            //Update cache
+
+            return new StoreBasketResult(command.Cart.UserName);
+
+        }
+    }
+}
